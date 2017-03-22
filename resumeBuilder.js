db0d0a0d565069e4b3f0ea3f42dd285b9f636f20
meght <meghanathotapalli@gmail.com>
2017-03-21 21:38:58 -0500

    Add resumebuilder

--- resumeBuilder.js
+++ resumeBuilder.js
@@ -36,59 +36,12 @@ var work = {
 				"detailed estimates as well as peer review of submitted work. I am also very involved in encouraging " +
 			"communication and collaboration within the team and have experience giving technical presentations as well " +
 			"as facilitating project retrospective discussions."
-		},
-		{
-			"employer": "MMM",
-			"title": "Systems Analyst",
-			"location": "Edmonton, Alberta, Canada",
-			"datesWorked": "September 2003 - April 2006",
-			"description": "I worked in a few different capacities here from internal application support of " +
-			"internal users using third party customized applications to working on internal software applications " +
-			"built using Oracle PL/SQL.  I also learned and used PHP to develop an internal web site to facilitate " +
-			"communication between the quality control and development teams by tracking builds, releases and issues."
-		},
-		{
-			"employer": "Matrikon",
-			"title": "Programmer",
-			"location": "Edmonton, Alberta, Canada",
-			"datesWorked": "March 2002 - September 2003",
-			"description": "I developed software for external clients in a variety of languages including C#.  This " +
-			"included design, development and deployment of back end systems as well as front end graphical user interfaces."
-		},
-		{
-			"employer": "University of Windsor",
-			"title": "Teaching Assisstant / Computer Centre Support / Computer Science Tutor",
-			"location": "Windsor, Ontario, Canada",
-			"datesWorked": "September 1998 - April 2001",
-			"description": "I supervised labs for the second year advanced programming class in C as a teaching " +
-			"assisstant.  At the University Computer Centre I helped students with use of the computing resources " +
-			"available.  As a computer science tutor I aided students with class work and helped them through problems."
-		},
-		
-		{
-			"employer": "Ontario Power Generation",
-			"title": "Programmer",
-			"location": "Kincardine, Ontario, Canada",
-			"datesWorked": "September 1999 - December 1999",
-			"description": "This was my third four month co-op placement through the University Co-op program. I " +
-			"developed virtual control room displays for operator training at the Kincardine Nuclear Power Plant."
-		},
-		{
-			"employer": "Cornerstone Technology",
-			"title": "Software Developer",
-			"location": "Windsor, Ontario, Canada",
-			"datesWorked": "May 1998 - April 1999",
-			"description": "I spent my first and second four month co-op placements here.  During my first placement I " +
-			"completed my given duties in less than half the time expected so I was able to work on developing a " +
-			"prototype of an expert system to design CAD drawings of injection moulds using CLIPS, a rules-based " +
-			"language.  I also had the opportunity to create a database management system using Delphi."
 		}
-	]
+           ]
 };
-
 var education = {
 	"schools": [
-		{ "name": "AAAr",
+		{ "name": "AAA",
 			"datesAttended": "1996 - 2001",
 			"location": "A,A,A",
 			"degree": "B.Sc.(Hon) Software Engineering",
@@ -98,15 +51,15 @@ var education = {
 		}		
 	],
 	"onlineCourses": [
-		{ "school": "Udacity",
+		{ "school": "Uuu",
 			"title": "Object-Oriented Javascript",
 			"completed": "November 2014",
-			"url": "https://www.udacity.com/course/ud015"
+			"url": "aaa"
 		},
 		{ "school": "Udacity",
 			"title": "HTML5 Canvas",
 			"completed": "November 2014",
-			"url": "https://www.udacity.com/course/ud292"
+			"url": "aaa"
 		}
 	]
 };
@@ -131,8 +84,6 @@ var projects = {
 		}
 	]
 };
-
-
 var formattedName = HTMLheaderName.replace("%data%", bio.name);
 var formattedRole = HTMLheaderRole.replace("%data%", bio.role);
 var formattedBioPic = HTMLbioPic.replace("%data%", bio.picture);
@@ -255,131 +206,4 @@ education.display = function() {
 }
 
 education.display();
-
-/**
- * Skills Chart
- */
-$(function () {
-    $('#container').highcharts({
-        chart: {
-            type: 'scatter',
-            zoomType: 'xy'
-        },
-        title: {
-            text: 'Skills'
-        },
-        xAxis: {
-            title: {
-                enabled: true,
-                text: 'Used (year)'
-            },
-            startOnTick: true,
-            endOnTick: true,
-            showLastLabel: true
-        },
-        yAxis: {
-            title: {
-                text: 'Duration (months)'
-            }
-        },
-        legend: {
-            layout: 'vertical',
-            align: 'left',
-            verticalAlign: 'top',
-            x: 250,
-            y: 0,
-            floating: true,
-            backgroundColor: (Highcharts.theme && Highcharts.theme.legendBackgroundColor) || '#FFFFFF',
-            borderWidth: 1
-        },
-        plotOptions: {
-            scatter: {
-                marker: {
-                    radius: 5,
-                    states: {
-                        hover: {
-                            enabled: true,
-                            lineColor: 'rgb(100,100,100)'
-                        }
-                    }
-                },
-                states: {
-                    hover: {
-                        marker: {
-                            enabled: false
-                        }
-                    }
-                },
-                tooltip: {
-                    headerFormat: '<b>{series.name}</b><br>',
-                    pointFormat: 'Used in {point.x} for {point.y} month(s)'
-                }
-			}
-		},
-		series: [{
-				name: 'Java',
-				color: 'rgba(223, 83, 83, .5)',
-				data: [[2014, 11], [2013, 11], [2012, 11], [2011, 8], [2010, 4], [2009, 12], [1998, 8], [1999, 8], [2000, 4]]
-			},
-			{
-				name: 'python',
-				color: 'rgba(119, 152, 191, .5)',
-				data: [[2014, 1], [2013, 1], [2012, 1], [2011, 1]]
-			},
-			{
-				name: 'SQL',
-				color: 'rgba(119, 152, 80, .5)',
-				data: [[2013, 2]]
-			},
-			{
-				name: 'PL/SQL',
-				color: 'rgba(170, 162, 30, .5)',
-				data: [[2010, 2], [2006, 4]]
-			},
-			{
-				name: 'C++',
-				color: 'rgba(0, 102, 0, .5)',
-				data: [[2004, 12], [2003, 6]]
-			},
-			{
-				name: 'C',
-				color: 'rgba(0, 102, 0, .5)',
-				data: [[1997, 8], [2004, 2], [2008, 4]]
-			},
-			{
-				name: 'Javascript',
-				color: 'rgba(204, 0, 0, .5)',
-				data: [[2014, 1]]
-			},
-			{
-				name: 'HTML/CSS',
-				color: 'rgba(153, 51, 0, .5)',
-				data: [[2014, 5], [2002, 3], [2008, 4], [2005, 2]]
-			},
-			{
-				name: 'Web Development',
-				color: 'rgba(0, 153, 102, .5)',
-				data: [[2014, 2]]
-			},
-			{
-				name: 'TDD',
-				color: 'rgba(0, 102, 51, .5)',
-				data: [[2014, 12], [2013, 12], [2012, 12], [2011, 4]]
-			},
-			{
-				name: 'Django',
-				color: 'rgba(204, 102, 0, .5)',
-				data: [[2013, 2.5]]
-			},
-			{
-				name: 'Perl',
-				color: 'rgba(210, 200, 0, .5)',
-				data: [[2011, 3], [2013, 1], [2012, 1], [2014, 1]]
-			}]
-	});
-});
-
-
-$("#mapDiv").append(googleMap);
-
 
